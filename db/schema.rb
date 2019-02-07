c1d62f12ea5e3fab754cf4769cdeaf988482c6ec
Yugandhartripathi <tripathi.yugandhar@gmail.com>
2019-02-07 08:07:25 +0530

    faker setup and all crud features with ests and admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_013749) do
+ActiveRecord::Schema.define(version: 20_190_207_020_738) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'password_digest'
+    t.string 'remember_digest'
+    t.boolean 'admin', default: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,46 +1,45 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:baseuser)
   end
 
-  test "login with invalid information" do
+  test 'login with invalid information' do
     get login_path
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: '', password: '' } }
     assert_not flash.empty?
     get root_path
     assert flash.empty?
   end
 
-  test "login with valid information followed by logout" do
+  test 'login with valid information followed by logout' do
     get login_path
-    post login_path, params: { session: { email:    @user.email,
+    post login_path, params: { session: { email: @user.email,
                                           password: 'password' } }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_response :success
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
+    assert_select 'a[href=?]', login_path, count: 0
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', user_path(@user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
     delete logout_path
     follow_redirect!
-    assert_select "a[href=?]", login_path, count: 1
-    assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select 'a[href=?]', login_path, count: 1
+    assert_select 'a[href=?]', logout_path, count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 
-  test "login with remember enabled" do
+  test 'login with remember enabled' do
     log_in_as(@user, remember_me: '1')
-    assert_equal cookies[:remember_token],assigns(:user).remember_token
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
   end
 
-  test "login with remember disabled" do
+  test 'login with remember disabled' do
     # Log in to set the cookie.
     log_in_as(@user, remember_me: '1')
     # Log in again and verify that the cookie is deleted.
